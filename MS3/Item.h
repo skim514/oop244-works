1a6cd5819000881df29da3220d1e157b1381a351
skim514 <skim514@myseneca.ca>
2023-06-05 22:43:02 -0400

    Project3

---
+++ MS3/Item.h
@@ -0,0 +1,56 @@
+﻿// Final Project Milestone 3
+//
+// Version 1.0
+// Date    2023-03-26
+// Author    Suna Kim
+// File name    Item.h
+#ifndef SDDS_ITEM_H_
+#define SDDS_ITEM_H_
+#include <iostream>
+#include "PosIO.h"
+#include "POS.h"
+#include "Error.h"
+
+using namespace std;
+
+namespace sdds{    
+    class Item : public PosIO
+    {
+        char m_SKU[MAX_SKU_LEN+1];
+        char* m_name;
+        double m_price;
+        bool m_isTaxed;
+        int m_qty;
+
+    protected:
+        int m_displayType;
+        Error m_error;
+        
+        
+    public:
+        Item();
+        Item(const Item& another);//Rule of three
+        Item& operator=(const Item& right);//Rule of three
+        ~Item();//Rule of three
+        bool operator==(const char* right) const;
+        bool operator>(const Item& right) const;
+        int operator+=(int right);
+        int operator-=(int right);
+        operator bool() const;
+        ostream& write(ostream& os) const;
+        ofstream& save(ofstream& os)const;
+        istream& read(istream& is);
+        ifstream& load(ifstream& is);
+        virtual char itemType() const = 0;
+        ostream& bprint(ostream& os) const;
+        void setEmpty();
+        
+        Item& displayType(int pos);
+        double cost() const;
+        int quantity() const;
+        Item& clear();
+    };
+    double operator+=(double& left, const Item& right);
+}
+
+#endif
---
+++ MS3/POS.h
@@ -0,0 +1,31 @@
+﻿// Final Project Milestone 3
+//
+// Version 1.0
+// Date    2023-03-26
+// Author    Suna Kim
+// File name    POS.h
+#ifndef SDDS_POS_H_
+#define SDDS_POS_H_
+
+using namespace std;
+
+namespace sdds{ //For constant values.
+    const double TAX = 0.13;
+    const int MAX_NAME_LEN = 40;
+    const int MAX_SKU_LEN = 7;
+    const int MAX_STOCK_NUMBER = 99;
+    const int POS_LIST = 1;
+    const int POS_FORM = 2;
+    
+    //char str[5] = { 0 };
+    //char str[] = "test";//이 자체가 사이즈임
+
+    const char ERROR_POS_SKU[] = "SKU too long";
+    const char ERROR_POS_NAME[] = "Item name too long";
+    const char ERROR_POS_PRICE[] = "Invalid price value";
+    const char ERROR_POS_TAX[] = "Invalid tax status";
+    const char ERROR_POS_QTY[] = "Invalid quantity value";
+    const char ERROR_POS_STOCK[] = "Item out of stock";
+    const char ERROR_POS_EMPTY[] = "Invalid Empty Item";
+}
+#endif
