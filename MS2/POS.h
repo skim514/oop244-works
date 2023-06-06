448eb9ec519ed4dbccd4f5e487b08a8334bcaced
skim514 <skim514@myseneca.ca>
2023-06-05 22:38:32 -0400

    Project2

---
+++ MS2/POS.h
@@ -0,0 +1,22 @@
+// Final Project Milestone 2
+//
+// Version 1.0
+// Date    2023-03-12
+// Author    Suna Kim
+// File name    POS.h
+#ifndef SDDS_POS_H_
+#define SDDS_POS_H_
+
+using namespace std;
+
+namespace sdds{ //For constant values.
+    const double TAX = 0.13;
+    const int MAX_SKU_LEN = 7;
+    const int MIN_YEAR = 2000;
+    const int MAX_YEAR = 2030;
+    const int MAX_STOCK_NUMBER = 99;
+    const int MAX_NO_ITEMS = 2000;
+}
+
+#endif
+
